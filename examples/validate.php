72de23daa1b4456fe13f01ec01954d75db2aa02b
rainbowray <rainbowray.str@gmail.com>
2017-09-21 21:22:05 +0500

    validate update

--- examples/validate.php
+++ examples/validate.php
@@ -26,6 +26,15 @@
       <table class="signup" border="0" cellpadding="2"
       cellspacing="5" bgcolor="#eeeeee">
       <th colspan="2" align="center">Registration form</th>
-        
+      <form method="post" action="adduser.php" onsubmit="return validate(this)">
+        <tr><td>Name</td><td><input type="text" maxlength="32" name="forename"></td></tr>
+        <tr><td>Surname</td><td><input type="text" maxlength="32" name="surname"></td></tr>
+        <tr><td>Username</td><td><input type="text" maxlength="16" name="username"></td></tr>
+        <tr><td>Password</td><td><input type="text" maxlength="12" name="password"></td></tr>
+        <tr><td>Age</td><td><input type="text" maxlength="3" name="age"></td></tr>
+        <tr><td>Email</td><td><input type="text" maxlength="64" name="email"></td></tr>
+        <tr><td colspan="2" align="center"><input type="submit" value="Sign Up"></td></tr>
+      </form>        
       </table>
     </body>
+  </html>
